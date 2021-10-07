1b106871fb1f8f9a843f9419ad9734e36449ab2c
brandonarude <brandon@rude.family>
2021-10-06 17:26:27 -0700

    Implementing ability to import data

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { default: createStrapi } = require('strapi');
+
 /**
  * An asynchronous bootstrap function that runs before
  * your application gets started.
@@ -10,4 +12,25 @@
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+function ImportCountry(data){
+    data.COUNTRIES.COUNTRY.forEach(element => {
+        console.log(element.country_id);
+        console.log(element.two_letter_code);
+        console.log(element["Country Name"]);
+        strapi.services.country.create({
+            country_id: element.country_id.toString(),
+            unique_id: element.country_id.toString(),
+            is_undisclosed: false,
+            two_letter_code: element.two_letter_code,
+            country_name: element["Country Name"]
+        });
+        
+    });
+};
+
+module.exports = async () => {
+    // Import Countries from .countryData.json in root directory
+    // let json = require('../../.countryData.json');
+    // ImportCountry(json);
+    
+};
