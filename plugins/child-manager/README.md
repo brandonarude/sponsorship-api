b83cdb9448fadc3558eb915d58e589050e24d040
brandonarude <brandon@rude.family>
2021-10-25 14:17:41 -0700

    Init custom plugin to manage children

---
+++ plugins/child-manager/README.md
@@ -0,0 +1,3 @@
+# Strapi plugin Child Manager
+
+A quick description of Child Manager.
---
+++ plugins/child-manager/controllers/child-manager.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * child-manager.js controller
+ *
+ * @description: A set of functions called "actions" of the `child-manager` plugin.
+ */
+
+module.exports = {
+
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  index: async (ctx) => {
+    // Add your own logic here.
+
+    // Send 200 `ok`
+    ctx.send({
+      message: 'ok'
+    });
+  }
+};
